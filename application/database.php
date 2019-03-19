a4980dd6c0e815911c3c9279ead4977da15d8c06
root <root@localhost.localdomain>
2019-03-19 17:01:11 +0800

    xk

--- application/database.php
+++ application/database.php
@@ -13,13 +13,13 @@ return [
     // 数据库类型
     'type'            => 'mysql',
     // 服务器地址
-    'hostname'        => '39.106.158.118',
+    'hostname'        => '',
     // 数据库名
-    'database'        => 'blog',
+    'database'        => '',
     // 用户名
-    'username'        => 'xk',
+    'username'        => '',
     // 密码
-    'password'        => 'lalala',
+    'password'        => '',
     // 端口
     'hostport'        => '',
     // 连接dsn
