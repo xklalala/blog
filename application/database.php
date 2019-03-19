2cc204964609c0e5fc7278bc03a9b83d6db03ebc
xk <2607780909@qq.com>
2019-03-19 17:12:57 +0800

    xk

--- application/database.php
+++ application/database.php
@@ -11,15 +11,15 @@
 
 return [
     // 数据库类型
-    'type'            => 'mysql',
+    'type'            => '',
     // 服务器地址
-    'hostname'        => '39.106.158.118',
+    'hostname'        => ,
     // 数据库名
-    'database'        => 'blog',
+    'database'        => '',
     // 用户名
-    'username'        => 'xk',
+    'username'        => '',
     // 密码
-    'password'        => 'lalala',
+    'password'        => '',
     // 端口
     'hostport'        => '',
     // 连接dsn
@@ -27,29 +27,29 @@ return [
     // 数据库连接参数
     'params'          => [],
     // 数据库编码默认采用utf8
-    'charset'         => 'utf8',
+    'charset'         => '',
     // 数据库表前缀
     'prefix'          => '',
     // 数据库调试模式
-    'debug'           => true,
+    'debug'           => ,
     // 数据库部署方式:0 集中式(单一服务器),1 分布式(主从服务器)
-    'deploy'          => 0,
+    'deploy'          => ,
     // 数据库读写是否分离 主从式有效
-    'rw_separate'     => false,
+    'rw_separate'     => ,
     // 读写分离后 主服务器数量
-    'master_num'      => 1,
+    'master_num'      => ,
     // 指定从服务器序号
     'slave_no'        => '',
     // 自动读取主库数据
-    'read_master'     => false,
+    'read_master'     => ,
     // 是否严格检查字段是否存在
-    'fields_strict'   => true,
+    'fields_strict'   => ,
     // 数据集返回类型
-    'resultset_type'  => 'array',
+    'resultset_type'  => '',
     // 自动写入时间戳字段
-    'auto_timestamp'  => false,
+    'auto_timestamp'  => ,
     // 时间字段取出后的默认时间格式
     'datetime_format' => 'Y-m-d H:i:s',
     // 是否需要进行SQL性能分析
-    'sql_explain'     => false,
+    'sql_explain'     => ,
 ];
